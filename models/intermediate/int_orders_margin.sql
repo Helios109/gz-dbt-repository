d7167334966e8d259f435659445c4481249f226b
Heledd Davies <heledd.davies@gmail.com>
2024-04-17 08:43:55 +0000

    Rounded Marigns for Orders

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,23 +1,17 @@
-with
-    sales_data as (
-        select
-            s.*,
-            p.purchase_price,
-            (
-                cast(s.quantity as float64) * cast(p.purchase_price as float64)
-            ) as purchase_cost
-        from `da-heledd-1633.dbt_hdavies.stg_raw__sales` s
-        join
-            `da-heledd-1633.dbt_hdavies.stg_raw__product` p
-            on s.products_id = p.products_id
-    )
+with sales_data as (
+    select
+        s.*,
+        p.purchase_price,
+        ROUND(cast(s.quantity as float64) * cast(p.purchase_price as float64), 2) as purchase_cost
+    from `da-heledd-1633.dbt_hdavies.stg_raw__sales` s
+    join `da-heledd-1633.dbt_hdavies.stg_raw__product` p
+    on s.products_id = p.products_id
+)
 
--- Select columns and calculate margin per sale
 select
     s.date_date,
-    sum(s.revenue) as total_revenue,
-    sum(s.purchase_cost) as total_purchase_cost,
-    sum(s.revenue - s.purchase_cost) as total_margin
+    ROUND(sum(s.revenue), 2) as total_revenue,
+    ROUND(sum(s.purchase_cost), 2) as total_purchase_cost,
+    ROUND(sum(s.revenue - s.purchase_cost), 2) as total_margin
 from sales_data s
 group by s.date_date
-
